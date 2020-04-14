f631ed4952600887b3adc4ab27f153c88a0660f1
Alex Fernández <alexfernandeznpm@gmail.com>
2020-04-14 23:53:42 +0200

    Use spaces.

--- whmcs/modules/servers/marketgoo/tokenCheck/getProductsFromAPI.php
+++ whmcs/modules/servers/marketgoo/tokenCheck/getProductsFromAPI.php
@@ -2,50 +2,50 @@
 
 function generateResponse()
 {
-	$postToken = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
-	if (!$postToken || $postToken == '')
-	{
-		$error = ["result" => "error", "message" => "Token not found"];
-		echo json_encode($error);
-		return;
-	}
-	require_once '../../../../init.php';
+    $postToken = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
+    if (!$postToken || $postToken == '')
+    {
+        $error = ["result" => "error", "message" => "Token not found"];
+        echo json_encode($error);
+        return;
+    }
+    require_once '../../../../init.php';
 
-	if (!file_exists('token.php'))
-	{
-		$error = ["result" => "error", "message" => "Token file not found!"];
-		echo json_encode($error);
-		return;
-	}
-	include 'token.php';
+    if (!file_exists('token.php'))
+    {
+        $error = ["result" => "error", "message" => "Token file not found!"];
+        echo json_encode($error);
+        return;
+    }
+    include 'token.php';
 
-	if (!$token || $token == '')
-	{
-		$error = ["result" => "error", "message" => "Cannot read the token!"];
-		echo json_encode($error);
-		return;
-	}
-	elseif ($postToken !== $token)
-	{
-		$error = ["result" => "error", "message" => "Received token not match module token!"];
-		echo json_encode($error);
-		return;
-	}
-	$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
-	if ($action == 'GetProducts')
-	{
+    if (!$token || $token == '')
+    {
+        $error = ["result" => "error", "message" => "Cannot read the token!"];
+        echo json_encode($error);
+        return;
+    }
+    elseif ($postToken !== $token)
+    {
+        $error = ["result" => "error", "message" => "Received token not match module token!"];
+        echo json_encode($error);
+        return;
+    }
+    $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+    if ($action == 'GetProducts')
+    {
         $products = getProducts();
 
         logModuleCall('marketgoo', $action, $postData, 'response', $products);
 
-		echo json_encode($products);
-	}
-	elseif ($action == 'GetClientsProducts')
+        echo json_encode($products);
+    }
+    elseif ($action == 'GetClientsProducts')
     {
         $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
         $postData = [
             'username2' => $username,
-		];
+        ];
         $clientProducts = localAPI($action, $postData);
         if ($clientProducts['result'] != 'success')
         {
@@ -65,7 +65,7 @@ function generateResponse()
         {
             $dict[$product['pid']] = $product;
         }
-            
+
         $filtered = [];
         foreach ($clientProducts['products']['product'] as $product)
         {
@@ -84,12 +84,12 @@ function generateResponse()
         logModuleCall('marketgoo', $action, $postData, 'response', $result);
 
         echo json_encode($result);
-	}
-	else
-	{
-		$error = ["result" => "error", "message" => "Unknown action"];
-		echo json_encode($error);
-	}
+    }
+    else
+    {
+        $error = ["result" => "error", "message" => "Unknown action"];
+        echo json_encode($error);
+    }
 }
 
 function getProducts()
