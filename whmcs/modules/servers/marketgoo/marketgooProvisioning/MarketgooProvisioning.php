e0dc093ff305a076268eb110a87e24e0929a8dcb
Alex Fernández on whmcs <alexfernandeznpm@gmail.com>
2020-04-15 11:21:09 +0000

    Corrected require.

--- whmcs/modules/servers/marketgoo/marketgooProvisioning/MarketgooProvisioning.php
+++ whmcs/modules/servers/marketgoo/marketgooProvisioning/MarketgooProvisioning.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../marketgooAPI/MarketgooAPI.php';
+require_once __DIR__ . '/../marketgooAPI/MarketgooAPI.php';
 
 /*
  * * ******************************************************************
